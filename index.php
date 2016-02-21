9a1afd15c1805767670b9a3955369b3363f73abf
kaotisk <ccc2007chaos@gmail.com>
2016-02-21 11:23:51 +0200

    some progress

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
-<?php 
-echo '<h1>welcome</h1>';
-echo '<p>hi! this is a front page for h.rethymno-meshnet.tk<br>';
-echo 'a site about the local mesh network in Rethymno, Greece</p>';
-echo '<pre>system under maintenance</pre>';
-
+<?php
+$message = '<h1>welcome</h1>';
+$about = '<p>hi! this is a front page for h.rethymno-meshnet.tk a site about the local mesh network in Rethymno, Greece</p>';
+$warnings = '<pre>system under maintenance</pre>';
 ?>
+<html>
+<head>
+<title>rethymno meshnet</title>
+<style>
+.logo {
+	text-align: center;
+}
+.about {}
+.special {
+	background-color: rgba(255,40,40,0.3);
+}
+.test {
+	color: green;
+	background-color: black;
+}
+</style>
+</head>
+<body>
+<div class="logo"><?php echo $message; ?></div>
+<div class="content"><?php echo $about; ?></div>
+<div class="special"><?php echo $warnings; ?></div>
+
+<div class="test"><?php include "public-node.php"; ?></div>
+</body>
+</html>
--- public-node.php
+++ public-node.php
@@ -1,10 +1,14 @@
 <?php
+
+$public_node = "<pre>
+\"rethymno-meshnet.tk:38295\": {
+            \"login\": \"default-login\",
+            \"password\":\"wgs9k7n7j5yh0kx7kyl5m7cpp71ls4y\",
+            \"publicKey\":\"wb3pt76psbt28mt9t2wzyudyh9zkqwq9z3jqb3t06y53g6f5qzh0.k\",
+            \"peerName\":\"your-name-goes-here\"
+        }
+</pre>";
+
+
+
 ?>
-<code>
-"192.168.1.11:38295": {
-            "login": "default-login",
-            "password":"wgs9k7n7j5yh0kx7kyl5m7cpp71ls4y",
-            "publicKey":"wb3pt76psbt28mt9t2wzyudyh9zkqwq9z3jqb3t06y53g6f5qzh0.k",
-            "peerName":"your-name-goes-here"
-        },
-</code>
