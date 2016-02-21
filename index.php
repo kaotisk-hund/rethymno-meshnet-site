3349495d329190f3f2581bc325e91b732290a791
kaotisk <ccc2007chaos@gmail.com>
2016-02-21 10:59:36 +0200

    maintenance

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo '<h1>welcome</h1>';
 echo '<p>hi! this is a front page for h.rethymno-meshnet.tk<br>';
 echo 'a site about the local mesh network in Rethymno, Greece</p>';
-
+echo '<pre>system under maintenance</pre>';
 
 ?>
