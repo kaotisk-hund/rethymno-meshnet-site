0b2a07a14b71b2002bb39e071c8c15cfe87c79ed
kaotisk <ccc2007chaos@gmail.com>
2016-03-03 13:03:01 +0200

    latest updates led to this commit

--- public-node.php
+++ public-node.php
@@ -2,11 +2,11 @@
 
 $public_node = "<pre>
 \"rethymno-meshnet.tk:38295\": {
-            \"login\": \"default-login\",
-            \"password\":\"wgs9k7n7j5yh0kx7kyl5m7cpp71ls4y\",
-            \"publicKey\":\"wb3pt76psbt28mt9t2wzyudyh9zkqwq9z3jqb3t06y53g6f5qzh0.k\",
-            \"peerName\":\"your-name-goes-here\"
-        }
+\t\"login\": \"default-login\",
+\t\"password\":\"wgs9k7n7j5yh0kx7kyl5m7cpp71ls4y\",
+\t\"publicKey\":\"wb3pt76psbt28mt9t2wzyudyh9zkqwq9z3jqb3t06y53g6f5qzh0.k\",
+\t\"peerName\":\"your-name-goes-here\"
+}
 </pre>";
 
 
